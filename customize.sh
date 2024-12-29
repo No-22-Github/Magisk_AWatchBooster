2b6848290be67b6c2b5c2e3a54dfc8ba6136fe77
No.22 <132265925+No-22-Github@users.noreply.github.com>
2024-12-30 01:53:29 +0800

    update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,10 +15,14 @@ echo -e "手表厂商：$(getprop ro.product.manufacturer)"
 echo -e "系统版本：$(getprop ro.build.display.id)"
 echo -e " 处理器 ：$(getprop ro.board.platform)"
 ui_print "- 正在释放文件"
-mkdir "/storage/emulated/0/Android/AWatchBooster"
+
+# 使用 -p 参数来确保目录已存在或成功创建
+mkdir -p "/storage/emulated/0/Android/AWatchBooster"
 ui_print "- 创建 AWatchBooster 文件夹"
+
 ui_print "- 配置文件与日志位于 /storage/emulated/0/Android/AWatchBooster"
 unzip -o "$ZIPFILE" 'config.yaml' -d "/storage/emulated/0/Android/AWatchBooster/" >&2
+
 echo "[$(date '+%m-%d %H:%M:%S.%3N')] AWatchBooster 模块安装成功, 等待重启" >> "/storage/emulated/0/Android/AWatchBooster/config.yaml.log"
 ui_print "- AWatchBooster 安卓手表通用优化模块"
 ui_print "- 开启无线 ADB / 解除安装限制"
