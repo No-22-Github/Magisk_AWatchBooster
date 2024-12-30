f69502d0c81a698780a55d3845b01c6ed7433996
No-22-Github <1829436840@qq.com>
2024-12-30 20:12:11 +0800

    update

--- modify_module_prop.sh
+++
@@ -1,8 +0,0 @@
-# 获取当前时间
-current_time=$(date "+%Y-%m-%d %H:%M:%S")
-
-# 在模块启动时删除之前的标记
-sed -i "s/\[.*🚀优化完毕\]//" $MODPATH/module.prop
-
-# 修改description，添加结束时间
-sed -i "s/^description=.*/& \[${current_time}🚀优化完毕\]/" $MODPATH/module.prop
